2be2b3173d7a9d5bf93fc17ce068f7141fc4788c
Kr-Gagandeo1025 <88143827+Kr-Gagandeo1025@users.noreply.github.com>
2024-03-17 12:32:22 +0530

    frontend basic style

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const port = 3000;
 const API_URL = "https://kodessphere-api.vercel.app";
 const teamID = "fqJgZK5"
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('views', './src/views');
+app.set("view engine","ejs");
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+})
 
 app.post("/post",async(req,res)=>{})
+
+app.listen(port,()=>{console.log(`app running on port ${port}`)});
