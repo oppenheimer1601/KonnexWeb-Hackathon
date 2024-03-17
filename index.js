78ced0144f1a2022ba1bb948fc6e2c6204909143
oppenheimer1601 <131872272+oppenheimer1601@users.noreply.github.com>
2024-03-17 12:36:00 +0530

    Revert "frontend basic style"

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const port = 3000;
 const API_URL = "https://kodessphere-api.vercel.app";
 const teamID = "fqJgZK5"
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
-app.set('views', './src/views');
-app.set("view engine","ejs");
-
-app.get("/", (req,res)=>{
-    res.render("index.ejs");
-})
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/post",async(req,res)=>{})
-
-app.listen(port,()=>{console.log(`app running on port ${port}`)});
