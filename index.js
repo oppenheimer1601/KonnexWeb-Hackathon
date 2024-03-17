7e2ef616ccf64a6895408492106de8501c047248
Aditya <adityaroy2169@gmail.com>
2024-03-17 12:31:18 +0530

    2nd updated index.js

--- index.js
+++ index.js
@@ -9,4 +9,26 @@ const teamID = "fqJgZK5"
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/post",async(req,res)=>{})
+
+
+app.post("/post", async (req, res) => {
+    const parm = {teamid : teamID,device :"fan",value : 2}
+    try {
+      const result = await axios.post(API_URL + "/devices/",parm);
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  app.get("/get",async (req,res)=>{
+try {
+    const result = await axios.get(API_URL + "/devices/");
+    console.log(result);
+} catch (err) {
+    console.log(err)
+}
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
